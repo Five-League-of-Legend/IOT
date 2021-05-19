f8d7aeec8a71b917c46dbd381a4cbc9c22079805
szh <szh@SZH>
2021-05-19 15:27:41 +0800

    添加项目文件。

---
+++ IOT.Core.IRepository/CommType/ICommTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOT.Core.IRepository.CommType
+{
+    public interface ICommTypeRepository:IBaseRepository<IOT.Core.Model.CommType>
+    {
+        List<IOT.Core.Model.CommType> UptState(int id);
+        int Uptss(Model.CommType c);
+    }
+}
---
+++ IOT.Core.IRepository/Commodity/ICommodityRepository.cs
@@ -0,0 +1,15 @@
+﻿using IOT.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOT.Core.IRepository.CommodityRepository
+{
+    public interface ICommodityRepository : IBaseRepository<IOT.Core.Model.Commodity>
+    {
+        int Uptstate(int id);
+        int Uptsstate(int id);
+    }
+}
